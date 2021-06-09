15c7ce1da21e1515a9d18ed461fd84d9d7cf87ea
vicmaburrito <vmaldaraca@gmail.com>
2021-06-09 15:00:08 -0500

    Add model user and validations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, length:{maximum: 12}, presence: true, uniqueness: true
+    validates :age, presence: true, numericality:{greater_than_or_equal_to: 13}
+    validates :email, presence: true, format:{with: /@/}
+end
