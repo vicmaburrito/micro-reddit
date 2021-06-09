84a7eca0ccad456569a963d48c8a94c12321f09b
JuliCarracedo <julicarra@hotmail.com>
2021-06-09 18:39:23 -0300

    add validators before migrate

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  validates :title, length:{maximum: 30}, presence: true
+  validates :body, length:{maximum: 100}, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+    validates :username, length:{maximum: 12}, presence: true, uniqueness: true
+    validates :age, presence: true, numericality:{greater_than_or_equal_to: 13}
+    validates :email, presence: true, format:{with: /@/}
+end
